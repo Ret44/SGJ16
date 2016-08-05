622b296eff6a272ae0665c36e420cef78cbd9d18
Bartłomiej Sieczka <sieczka.bartlomiej@gmail.com>
2016-08-05 22:07:44 +0200

    no message

---
+++ Src/Assets/Scripts/CrowdController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CrowdController : MonoBehaviour {
+
+    public static CrowdController instance;
+    public List<GameObject> dummies;
+
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
