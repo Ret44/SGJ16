8495030a85aa7d2b49b211230d7f01d5890587fa
filipw <wrobelfilip@gmail.com>
2016-08-06 23:57:02 +0200

    [*] Fixed menu

--- Src/Assets/Scripts/GUI/Menu/MenuController_Credits.cs
+++ Src/Assets/Scripts/GUI/Menu/MenuController_Credits.cs
@@ -1,17 +1,37 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class MenuController_Credits : MonoBehaviour
 {
 	#region Variables
 
+	[SerializeField]
+	private Text _creditsText = null;
+
+	[SerializeField]
+	private TextAsset _creditsTextFile = null;
+
 	#endregion Variables
 
 	#region Monobehaviour Methods
 
+	void OnEnable()
+	{
+		ResetMenuController_Credits();
+    }
+
 	#endregion Monobehaviour Methods
 
 	#region Methods
+
+	private void ResetMenuController_Credits()
+	{
+		if(_creditsText != null && _creditsTextFile != null)
+		{
+			_creditsText.text = _creditsTextFile.text;
+		}
+	}
 
 	public void ClickBack()
 	{
